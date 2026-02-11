1f2359a2f0cea1410f57044cba50a3139b5dea25
C@:\EM1NAL <emaperry@stupidcat.uk>
2026-02-11 01:08:45 +0000

    Delete APyday 2 directory

--- APyday 2/lua/load_unlocks.lua
+++
@@ -1,5 +0,0 @@
-log("Attempting apd2_data load...")
-apd2_data = io.load_as_json("mods/saves/apyday2.txt") or {}
-apd2_default_upgrades = {"jowi","x_1911","x_b92fs","x_deagle","x_g22c",
-    "x_g17","x_usp","x_sr2","x_mp5","x_akmsu","x_packrat","x_p226","x_m45",
-    "x_mp7","x_ppk","player_special_enemy_highlight","player_hostage_trade"}
--- APyday 2/lua/tweakdata/narrative.lua
+++
@@ -1,9 +0,0 @@
-Hooks:PostHook(NarrativeTweakData, "is_job_locked", "apd2_heists", function(self, job_id)
-    if apd2_data.heists and apd2_data.heists[job_id] then
-        log("Unlocking " .. job_id)
-        return false
-    else
-        log("Locking " .. job_id)
-        return true
-    end
-end)
