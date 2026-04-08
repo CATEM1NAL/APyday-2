56d51e4519ae8ba9751b88f9447623f7424b7b59
C@:\EM1NAL <emaperry@stupidcat.uk>
2026-04-08 21:46:35 +0100

    Delete Criminal Dawn directory

--- Criminal Dawn/lua/managers/dlc.lua
+++
@@ -1,6 +0,0 @@
--- Unlock content that is inaccessible in Criminal Dawn & achievement locked content that impacts gameplay
-Hooks:OverrideFunction(GenericDLCManager, "has_pd2_clan", function() return true end)
-Hooks:OverrideFunction(GenericDLCManager, "is_content_skirmish_locked", function() return false end)
-Hooks:OverrideFunction(GenericDLCManager, "is_content_crimespree_locked", function() return false end)
-Hooks:OverrideFunction(GenericDLCManager, "is_weapon_mod_achievement_locked", function() return false end)
-Hooks:OverrideFunction(GenericDLCManager, "is_weapon_mod_achievement_milestone_locked", function() return false end)
