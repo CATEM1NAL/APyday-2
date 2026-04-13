db04532e9b8fdfb7b1ce0b2c206f80f85237d572
C@:\EM1NAL <emaperry@stupidcat.uk>
2026-04-13 22:56:49 +0100

    Delete Criminal Dawn/managers directory

--- Criminal Dawn/managers/infamy.lua
+++
@@ -1,5 +0,0 @@
-Hooks:OverrideFunction(InfamyManager, "available", function() return true end)
-
-Hooks:PreHook(InfamyManager, "load", "CrimDawn_InfamyLoad", function(self)
-  for _, item in ipairs(tweak_data.infamy.tree) do self:unlock_item(item) end
-end)
