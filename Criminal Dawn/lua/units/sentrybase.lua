375d46edaf539a9252d199c5ae4f31961d04346e
C@:\EM1NAL <emaperry@stupidcat.uk>
2026-04-29 01:15:07 +0100

    Delete Criminal Dawn/lua/units/sentrybase.lua

--- Criminal Dawn/lua/units/sentrybase.lua
+++
@@ -1,4 +0,0 @@
-Hooks:PostHook(SentryGunBase, "activate_as_module", "CrimDawn_SentryActivate", function(self, team_type, tweak_table_id)
-  log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-  --self._unit:brain():set_active(false)
-end)
